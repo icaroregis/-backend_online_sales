d8cc38678d02ce4e9e810bd48ec45801edabbc96
icaro_regis <progalmeidaregis@outlook.com>
2023-03-21 23:34:12 -0300

    feat: service (GET), service create(POST)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get()
-  async getAllUsers() {
-    return JSON.stringify({ test: 'abc' });
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.get();
   }
 
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
-    return {
-      ...createUser,
-      password: undefined,
-    };
+  async createUser(@Body() createUser: CreateUserDto): Promise<User> {
+    return this.userService.create(createUser);
   }
 }
